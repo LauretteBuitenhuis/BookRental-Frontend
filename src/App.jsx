edee8403b3cccb87c9251d9e1d01c2d47878850f
lisalootens <lisa_lootens@live.nl>
2022-12-21 17:35:09 +0100

    Create createUser page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Login from "./components/Login"
 import Main from "./components/Main"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import createUser from "./components/CreateUser"
 
 function App() {
   return (
     <div className="app-container">
       <Header/>
       <Routes>
-        <Route path="/" element={ <Login/> } />
-        <Route path="main" element={ <Main/> } />
+        <Route path="/" element={ <Login/> }/>
+        <Route path="main" element={ <Main/> }/>
+        <Route path="/create" element={ <createUser/> }/>
       </Routes>
       <Footer/>
     </div>
