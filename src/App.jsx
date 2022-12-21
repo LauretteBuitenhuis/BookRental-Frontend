ac020a9b4d8d8d6bacf6d6424399522a9c56579f
lisalootens <lisa_lootens@live.nl>
2022-12-21 18:42:26 +0100

    Change page title in index.html to Book Rental and removed unnecessary comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Login from "./components/Login"
 import Main from "./components/Main"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import NewUser from "./components/NewUser"
+import newUser from "./components/newUser"
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Login/> }/>
         <Route path="main" element={ <Main/> }/>
-        <Route path="/register" element={ <NewUser/> }/>
+        <Route path="/register" element={ <newUser/> }/>
       </Routes>
       <Footer/>
     </div>
