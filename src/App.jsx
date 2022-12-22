0354efc0928c73c55d17313a2b4fd77b3a4f6ca4
lisalootens <lisa_lootens@live.nl>
2022-12-22 07:53:47 +0100

    Add create user form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom"
+import './styles/index.css';
 import Login from "./components/Login"
 import Main from "./components/Main"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import newUser from "./components/newUser"
+import NewUser from "./components/newUser"
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Login/> }/>
         <Route path="main" element={ <Main/> }/>
-        <Route path="/register" element={ <newUser/> }/>
+        <Route path="/register" element={ <NewUser/> }/>
       </Routes>
       <Footer/>
     </div>
--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import "../styles/newUser.css"
 
-export default function newUser() {
-  return (
-    <div>
-    </div>
-  )
+export function NewUser() {
+    return (
+      <div class="container">
+        <h2>Nieuwe gebruiker</h2>
+
+      <form>
+        <input type="text" placeholder="Voornaam"></input>
+        <input type="text" placeholder="Achternaam"></input>
+        <input type="text" placeholder="Email"></input>
+        <input type="text" placeholder="Wachtwoord"></input>
+
+        <label for="is-admin-checkbox">
+        <input id="is-admin-checkbox" type="checkbox" placeholder="Admin"></input>
+          Admin</label>
+        
+
+        <button type="submit" class="button">Toevoegen</button>
+      </form>
+
+      </div>
+    )
 }
