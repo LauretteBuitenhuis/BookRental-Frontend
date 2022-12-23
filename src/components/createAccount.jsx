d6ff73a9d503a09935c41662a1e1b91d82af4a77
lisalootens <lisa_lootens@live.nl>
2022-12-23 14:11:14 +0100

    Define consts

--- src/components/createAccount.jsx
+++ src/components/createAccount.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/createAccount.css"
 
 export function CreateAccount() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [admin, setAdmin] = useState('');
+
     return (
       <div className="createaccount-container">
         <h2>Nieuwe gebruiker</h2>
 
       <form className="createaccount-form">
-        <input className="createacccount-input" type="text" placeholder="Voornaam"></input>
-        <input className="createacccount-input" type="text" placeholder="Achternaam"></input>
-        <input className="createacccount-input" type="text" placeholder="Email"></input>
-        <input className="createacccount-input" type="text" placeholder="Wachtwoord"></input>
+        <input className="createacccount-input" type="text" placeholder="Voornaam" value={firstName} onChange={(e)=>setFirstName(e.target.value)} required></input>
+        <input className="createacccount-input" type="text" placeholder="Achternaam" value={lastName} onChange={(e)=>setLastName(e.target.value)}  required></input>
+        <input className="createacccount-input" type="text" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}  required></input>
+        <input className="createacccount-input" type="password" placeholder="Wachtwoord" autoComplete="off" value={password} onChange={(e)=>setPassword(e.target.value)} required></input>
 
         <label htmlFor="is-admin-checkbox" className="createaccount-label">
         <input id="is-admin-checkbox" type="checkbox" className="createacccount-input"></input>
@@ -18,7 +24,6 @@ export function CreateAccount() {
 
         <button type="submit" className="createaccount-button">Aanmaken</button>
       </form>
-
       </div>
     )
 }
