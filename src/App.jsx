669b98f349f1a7ba74fe84a64944a44661029645
Laurette Buitenhuis <laurettebuitenhuis@hotmail.com>
2022-12-29 18:07:31 +0100

    removed footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route, Navigate} from "react-router-dom"
 import Login from "./components/Login"
 import Main from "./components/Main"
 import Header from "./components/Header"
-import Footer from "./components/Footer"
 import AuthContext from "./Store/auth-context"
 
 
@@ -22,7 +21,6 @@ function App() {
           
           </Route> 
       </Routes>
-      <Footer/>
     </div>
   )
 }
