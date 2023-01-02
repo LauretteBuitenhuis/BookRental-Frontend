fa4175fa8eea1f8a0071644addae46c5e37dd66d
Laurette Buitenhuis <laurettebuitenhuis@hotmail.com>
2023-01-02 11:51:45 +0100

    Added extra logout button to avoid getting stuck during development...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from '../assets/0012p000041aQ4LAAU.png'
+import AuthContext from '../Store/auth-context';
 
 function Header() {
+  // TODO: remove logout button for development purposes
+  const navigate = useNavigate();
+    const authCtx = useContext(AuthContext);
+
+    const logoutHandler = () => {
+        authCtx.logout();
+        navigate('/', { replace: true });
+    }
+
 
   return (
     <div className="header">
       <img src={logo} alt='Header' className='header-img' />
-    </div>
+    <div>
+      <button className='logout-button' onClick={logoutHandler} > logout</button>
+      </div> </div>
   )
 };
 
