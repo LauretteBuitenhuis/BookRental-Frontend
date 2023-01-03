a801a7c4436cc0c4d8fa83bd3377b3b571931081
Laurette Buitenhuis <laurettebuitenhuis@hotmail.com>
2023-01-03 15:18:10 +0100

    Added user button and redirect to book inventory user

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -3,31 +3,40 @@ import { useNavigate } from "react-router-dom";
 import AuthContext from "../store/auth-context";
 import logOutIcon from '../assets/ic_exit_to_app_24px.png';
 import UserIcon from '../assets/ic_account_box_24px_user.png';
+import BookInventory from '../assets/ic_add_to_photos_24px.png';
 import '../styles/mainUser.css';
 
 
 function UserPage() {
-  const navigate = useNavigate();
-  const authCtx = useContext(AuthContext);
+    const navigate = useNavigate();
+    const authCtx = useContext(AuthContext);
 
-  const logoutHandler = () => {
-    authCtx.logout();
-    navigate("/", { replace: true });
-  };
+    const logoutHandler = () => {
+        authCtx.logout();
+        navigate("/", { replace: true });
+    };
 
-  return (
-    <div className="userheader-container">
-      <div className="userheader">
-        <h4>OVERZICHT</h4>
-        <h2>Reserveringen</h2>
-      </div>
+    const bookInventoryHandler = () => {
+        navigate("/books", { replace: true });
+    };
 
-            <nav className='user-navbar'>
-                <button><img src={UserIcon} alt='Admin'/></button>
-                <span>Voornaam Achternaam</span>                
-                <button><img src={logOutIcon} alt='log out' onClick={logoutHandler}/></button>
-            </nav>
+    return (
+        <div>
+            <div className="userheader-container">
+                <div className="userheader">
+                    <h4>OVERZICHT</h4>
+                    <h2>Reserveringen</h2>
+                </div>
+
+                <nav className='user-navbar'>
+                    <button><img src={UserIcon} alt='Admin' /></button>
+                    <span>Voornaam Achternaam</span>
+                    <button><img src={logOutIcon} alt='log out' onClick={logoutHandler} /></button>
+                </nav>
+            </div>
+            <img src={BookInventory} alt='Books' onClick={bookInventoryHandler} />
         </div>
+
     )
 
 }
