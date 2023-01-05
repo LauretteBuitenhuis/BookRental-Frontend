8f83dde9ea27aa41c7afef403ec9dbf60a563ec5
lisalootens <lisa_lootens@live.nl>
2023-01-05 09:59:38 +0100

    Remove double code

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,28 +1,10 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import AuthContext from "../store/auth-context";
-import logOutIcon from "../assets/ic_exit_to_app_24px.png";
-import EmployeesIcon from "../assets/ic_supervisor_account_24px.png";
-import InventoryIcon from "../assets/BooksOverview.png";
-import AdminIcon from "../assets/ic_account_box_24px_admin.png";
+
 import "../styles/mainAdmin.css";
 
 function AdminPage() {
-  const navigate = useNavigate();
   const authCtx = useContext(AuthContext);
-
-  const logoutHandler = () => {
-    authCtx.logout();
-    navigate("/", { replace: true });
-  };
-
-  const bookInventoryHandler = () => {
-    navigate("/inventory", { replace: true });
-  };
-
-  const employeesHandler = () => {
-    navigate("/employees", { replace: true });
-  };
 
   return (
     <div className="adminheader-container">
@@ -30,30 +12,6 @@ function AdminPage() {
         <h4>OVERZICHT</h4>
         <h2>Reserveringen</h2>
       </div>
-
-      <nav className="navbar">
-        <img src={AdminIcon} alt="Admin" />
-        <span>Voornaam Achternaam</span>
-        <button>
-          {" "}
-          <img
-            src={InventoryIcon}
-            alt="Boeken inventaris"
-            onClick={bookInventoryHandler}
-          />
-        </button>
-        <button>
-          {" "}
-          <img
-            src={EmployeesIcon}
-            alt="Werknemers"
-            onClick={employeesHandler}
-          />
-        </button>
-        <button>
-          <img src={logOutIcon} alt="log out" onClick={logoutHandler} />
-        </button>
-      </nav>
     </div>
   );
 }
--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,61 +1,32 @@
-import { useContext } from "react";
 import { useNavigate } from "react-router";
 import { MdLibraryAdd } from "react-icons/md";
-import AuthContext from "../store/auth-context";
-import logOutIcon from '../assets/ic_exit_to_app_24px.png';
-import EmployeesIcon from '../assets/ic_supervisor_account_24px.png';
-import InventoryIcon from '../assets/BooksOverview.png';
-import AdminIcon from '../assets/ic_account_box_24px_admin.png';
-import '../styles/employees.css';
-
-
-
+import "../styles/employees.css";
 
 function Employees() {
-    const navigate = useNavigate();
-    const authCtx = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const logoutHandler = () => {
-        authCtx.logout();
-        navigate("/", { replace: true });
-    };
+  const createUserHandler = () => {
+    navigate("/register", { replace: true });
+  };
 
-    const bookInventoryHandler = () => {
-        navigate("/books", { replace: true });
-    }
-
-
-    const createUserHandler = () => {
-        navigate("/register", { replace: true });
-    };
-
-    return (
-        <div>
-            <div>
-
-                <div className="employeesheader-container">
-                    <div className="employeesheader">
-                        <h4>BEKIJK WERKNEMERS</h4>
-                        <h2>Werknemersoverzicht</h2>
-                    </div>
-
-                    <nav className="employeesnavbar">
-                        <img src={AdminIcon} alt='Admin' />
-                        <span>Voornaam Achternaam</span>
-                        <button> <img src={InventoryIcon} alt='Boeken inventaris' onClick={bookInventoryHandler} /></button>
-                        <img className="static" src={EmployeesIcon} alt='Werknemers' />
-                        <button><img src={logOutIcon} alt='log out' onClick={logoutHandler} /></button>
-                    </nav>
-                </div>
-            </div>
-            <div>
-                <h5><MdLibraryAdd className="icon" onClick={createUserHandler} />Voeg werknemer toe </h5>
-            </div>
-
-
+  return (
+    <div>
+      <div>
+        <div className="employeesheader-container">
+          <div className="employeesheader">
+            <h4>BEKIJK WERKNEMERS</h4>
+            <h2>Werknemersoverzicht</h2>
+          </div>
         </div>
-
-    )
+      </div>
+      <div>
+        <h5>
+          <MdLibraryAdd className="icon" onClick={createUserHandler} />
+          Voeg werknemer toe{" "}
+        </h5>
+      </div>
+    </div>
+  );
 }
 
-export default Employees
+export default Employees;
