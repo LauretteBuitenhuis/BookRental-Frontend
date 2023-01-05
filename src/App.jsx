e5fb4b8ebf31b6555435432336c282caa1eaf9ea
lisalootens <lisa_lootens@live.nl>
2023-01-05 10:19:24 +0100

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,32 @@ import Header from "./components/Header";
 import AuthContext from "./store/auth-context";
 import { Inventory } from "./components/Inventory";
 import Employees from "./components/Employees";
+import Footer from "./components/Footer";
 
 function App() {
   const authCtx = useContext(AuthContext);
   const isLoggedIn = authCtx.isLoggedIn();
 
   return (
-    <div className="app-container">
+    <div>
       <Header />
       <Routes>
         {!isLoggedIn && <Route path="/" element={<Login />} />}
         {isLoggedIn && <Route path="/main" element={<Main />} />}
-        {isLoggedIn && authCtx.isAdmin && <Route path="/register" element={<CreateAccount />} />}
+        {isLoggedIn && authCtx.isAdmin && (
+          <Route path="/register" element={<CreateAccount />} />
+        )}
         {isLoggedIn && <Route path="/inventory" element={<Inventory />} />}
-        {isLoggedIn && authCtx.isAdmin && <Route path="employees" element={<Employees />} />}
+        {isLoggedIn && authCtx.isAdmin && (
+          <Route path="employees" element={<Employees />} />
+        )}
         {isLoggedIn && <Route path="reservations" element={<Reservations />} />}
         <Route
           path="*"
           element={<Navigate to={isLoggedIn ? "/main" : "/"} replace />}
         />
       </Routes>
+      <Footer />
     </div>
   );
 }
